ac49246fbb6de85550d0c8b16feb260df7fae04c
jafetsierra <jafet.sierra.l@gmail.com>
2022-01-10 19:04:15 -0500

    entire frontend app

---
+++ src/App.vue
@@ -0,0 +1,146 @@
+<template>
+  <div id="app" class="app">
+    <div class="header">
+      <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-2 mb-3 border-bottom">
+          <a href="/" class="d-flex align-items-center mb-1 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <a class="navbar-brand" href="#">
+              <img src="./assets/diabetesLogo.png" alt="" width="90" height="70" class="d-inline-block align-text-center">
+
+            </a>
+          </a>
+          <ul class="nav nav-pills">
+            <li class="nav-item"><button type="button" class="btn btn-outline-primary" v-if="is_auth" v-on:click="loadHome">Home</button></li>
+            <li class="nav-item"><button type="button" class="btn btn-outline-success" v-if="is_auth" v-on:click="loadPrediction">Make a Diagnosis</button></li>
+            <li class="nav-item"><button type="button" class="btn btn-outline-success" v-if="is_auth" v-on:click="loadPredictions">My Diagnostics</button></li>
+            <li class="nav-item"><button type="button" class="btn btn-outline-danger" v-if="is_auth" v-on:click="logOut">LogOut</button></li>
+            <li class="nav-item"><button type="button" class="btn btn-outline-primary" v-if="!is_auth" v-on:click="loadLogIn">LogIn</button></li>
+            <li class="nav-item"><button type="button" class="btn btn-outline-success" v-if="!is_auth" v-on:click="loadSignUp">SignUp</button></li>
+          </ul>
+        </header>
+      </div>
+    </div>
+    <div class="main-component">
+      <router-view
+        v-on:completedLogIn="completedLogIn"
+        v-on:completedSignUp="completedSignUp"
+        v-on:logOut="logOut"
+        >
+      </router-view>
+    </div>
+
+    <div class="footer">
+      <h2>Machine Learning algorithms for Diabetes Diagnosis</h2>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: function(){
+    return{
+      is_auth: false
+    }
+  },
+  components: {
+  },
+  methods:{
+    loadLogIn: function(){
+      this.$router.push({name: "logIn"})
+    },
+    loadSignUp: function(){
+      this.$router.push({name: "signUp"})
+    },
+    completedLogIn: function(data) {
+      localStorage.setItem("isAuth", true);
+      localStorage.setItem("username", data.username);
+      localStorage.setItem("token_access", data.token_access);
+      localStorage.setItem("token_refresh", data.token_refresh);
+      alert("Autenticación Exitosa");
+      this.verifyAuth();
+    },
+    completedSignUp: function(data) {
+      alert("Registro Exitoso");
+      this.completedLogIn(data);
+    },
+    loadHome: function() {
+      this.$router.push({ name: "home" });
+    },
+    loadPrediction: function() {
+      this.$router.push({ name: "prediction" });
+    },
+    completedPrediction: function(data) {
+
+      alert("Registro Exitoso");
+    },
+    loadPredictions: function() {
+      this.$router.push({ name: "predictionlist" });
+    },
+    logOut: function () {
+      localStorage.clear();
+      alert("Sesión Cerrada");
+      this.verifyAuth();
+    },
+    verifyAuth: function() {
+      this.is_auth = localStorage.getItem("isAuth") || false;
+
+      if (this.is_auth == false)
+        this.$router.push({ name: "logIn" });
+      else
+        this.$router.push({ name: "home" });
+    }
+  },
+  created: function(){
+    this.verifyAuth()
+  }
+}
+</script> 
+
+<style>
+body{
+  margin: 0 0 0 0;
+}
+.app{
+  background-color:#1158a3;
+}
+.header{
+  align-items: center;
+  padding: 0;
+  margin-bottom:0px;
+  background-color:rgb(218, 224, 255);
+}
+.header h1{
+width: 20%;
+text-align: center;
+}
+.header nav {
+  align-items: center;
+  padding-top: 50%;
+  
+}
+.main-component{
+  height: 75vh;
+  margin: 0%;
+  padding: 0%;
+  background: #FDFEFE ;
+}
+.footer{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color: #1158a3;
+  color: #E5E7E9;
+}
+.footer h2{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="greetings">
+        <h3>
+            <h2>V-1.0 of the web aplication for Diabetes Diagnosis using the Machine Learning algorithm xgboost.  </h2> 
+            <br>
+            <br>
+            <p>Built and developed by JafetSierra</p>
+            <br>
+            <p>You can see the entire project in the link given below</p>
+            <br>
+            <a href="https://github.com/jafetsierra?tab=repositories"> GitHub </a>
+            <br>
+            <br>
+            <p>If you want to check out the trained model, go to the next Google Colab Notebook</p>
+            <a href="https://colab.research.google.com/drive/1S5_J49_ZVXLKGtxMREM1E-bDCBEGEDL8#scrollTo=ZzFH3JHTwqA8">Notebook</a>
+
+        </h3>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Home",
+    data: function(){
+        return {
+            username: localStorage.getItem('username') || "none",
+            img1: 'src/assets/logo.jpg'
+        }
+    }
+}
+</script>
+
+<style>
+.greetings{
+    margin: 0;
+    padding: 0%;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+.greetings h1{
+    font-size: 50px;
+    color: #283747;
+}
+.greetings span{
+    color: crimson;
+    font-weight: bold;
+}
+</style>
